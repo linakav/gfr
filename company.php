0dbcee706591a5ed7603748c0db634f6d142f7be
Lina <lina_kav@msn.com>
2017-02-07 10:34:17 +0200

    no message

--- company.php
+++ company.php
@@ -13,7 +13,17 @@ $lang = $_GET["lang"];
             <img src="/images/smallbanner4.jpg" alt="img01">
             
             <?php if ($lang == "en")
-                    echo "";
+                    echo "<p>The company <strong>GEORGE MAVRIANOS CO LP</strong>, with the distinctive title <strong>GFR PARTS & SPARES</strong>, was founded in 2016 and deals in car parts of all types, both domestic and imported.</p>"
+                . "Our company consists of specialized staff and is housed in Elefsina, with collaborating shops in Lykovrysi and Patisia, Attika.</p><p>"
+                . "Our goal is serving our clients with effectiveness, speed, and products that combine the best quality with the best price.</p><p>"
+                . "OUR PERSONAL CULTURE :"
+                    . "<ul><li>Reliability & Honesty</li>"
+                    . "<li>Respect & Appreciation</li>"
+                    . "<li>Accountability</li>"
+                    . "<li>ETHICAL TRADING</li>"
+                    . "<li>Integrity</li>"
+                    . "<li>Trust</li>"
+                    . "<li>Consistency</li><ul></p>";
                 else {
                     echo "<p>Η εταιρεία <strong>ΓΕΩΡΓΙΟΣ ΜΑΥΡΙΑΝΟΣ & ΣΙΑ ΕΕ</strong> , με διακριτικό τίτλο: <strong>GFR PARTS & SPARES</strong>, ιδρύθηκε το 2016 με κύριο αντικείμενο την εμπορία ανταλλακτικών αυτοκινήτου παντός τύπου, με χρήση της εγχώριας αγοράς και εισαγωγών.</p><p>
 Η εταιρεία μας στελεχώνεται από εξειδικευμένο προσωπικό και στεγάζεται στην Ελευσίνα, με συνεργαζόμενα καταστήματα στην Λυκόβρυση και στα Πατήσια Αττικής.</p><p>
